5db6c578a519e3a239e92afba0af95fdee6fb994
Zach Smith <zach.smith.63044@gmail.com>
2026-07-23 15:54:36 +0100

    Add pgcat integration: append pool blocks and reload on tenant creation (fixed)

--- provisioner.js
+++ provisioner.js
@@ -3,7 +3,7 @@ import path from 'path';
 import { execSync } from 'child_process';
 import crypto from 'crypto';
 import { fileURLToPath } from 'url';
-import { addTenantToPgcat } from 'pgcatHandler'
+import { addTenantToPgcat } from './pgcatHandler.js'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
